e3fd030e193eaeb3fea2dca1b064d2283790d714
raghunayyar <me@iraghu.com>
2015-02-10 23:04:19 +0530

    WIP: Adds Grids for half widgets.

--- application/views/widgets/widget2.php
+++ application/views/widgets/widget2.php
@@ -1,14 +1,35 @@
 <!-- Recently Added Glyph Widget -->
-<div class="col-md-6 well widget">
+<div class="col-md-6 well widget widget-half" id="recentglyphwidget">
 	<div class="widget-header widget-header-custom">
 		<h3>Recently Added Glyphs</h3>
 	</div>
-	<ul>
+	<ul class="widget-element-list row">
 		<li>
-			<div class="leftcontent">
+			<div class="leftcontent col-sm-3">
 				<img src="http://placehold.it/100x100" class="glyph" />
 			</div>
-			<div class="rightcontent">
+			<div class="rightcontent col-sm-9">
+				<div class="combination widgetgroup">
+					<span class="letter">A</span>
+					<span class="combinator">+</span>
+					<span class="letter">B</span>
+				</div>
+				<div class="widgetgroup">
+					<span class="label">Language</span>
+					<span class="value">Hindi, Marathi</span>
+				</div>
+				<div class="widgetgroup">
+					<span class="label">Usercase:</span>
+					<span class="value">TheWord</span>
+				</div>
+			</div>
+			<div class="clear"></div>
+		</li>
+		<li>
+			<div class="leftcontent col-sm-3">
+				<img src="http://placehold.it/100x100" class="glyph" />
+			</div>
+			<div class="leftcontent col-sm-9">
 				<div class="combination widgetgroup">
 					<span class="letter">A</span>
 					<span class="combinator">+</span>
@@ -25,10 +46,10 @@
 			</div>
 		</li>
 		<li>
-			<div class="leftcontent">
+			<div class="leftcontent col-sm-3">
 				<img src="http://placehold.it/100x100" class="glyph" />
 			</div>
-			<div class="rightcontent">
+			<div class="leftcontent col-sm-9">
 				<div class="combination widgetgroup">
 					<span class="letter">A</span>
 					<span class="combinator">+</span>
@@ -44,65 +65,5 @@
 				</div>
 			</div>
 		</li>
-		<li>
-			<div class="leftcontent">
-				<img src="http://placehold.it/100x100" class="glyph" />
-			</div>
-			<div class="rightcontent">
-				<div class="combination widgetgroup">
-					<span class="letter">A</span>
-					<span class="combinator">+</span>
-					<span class="letter">B</span>
-				</div>
-				<div class="widgetgroup">
-					<span class="label">Language</span>
-					<span class="value">Hindi, Marathi</span>
-				</div>
-				<div class="widgetgroup">
-					<span class="label">Usercase:</span>
-					<span class="value">TheWord</span>
-				</div>
-			</div>
-		</li>
-		<li>
-			<div class="leftcontent">
-				<img src="http://placehold.it/100x100" class="glyph" />
-			</div>
-			<div class="rightcontent">
-				<div class="combination widgetgroup">
-					<span class="letter">A</span>
-					<span class="combinator">+</span>
-					<span class="letter">B</span>
-				</div>
-				<div class="widgetgroup">
-					<span class="label">Language</span>
-					<span class="value">Hindi, Marathi</span>
-				</div>
-				<div class="widgetgroup">
-					<span class="label">Usercase:</span>
-					<span class="value">TheWord</span>
-				</div>
-			</div>
-		</li>
-		<li>
-			<div class="leftcontent">
-				<img src="http://placehold.it/100x100" class="glyph" />
-			</div>
-			<div class="rightcontent">
-				<div class="combination widgetgroup">
-					<span class="letter">A</span>
-					<span class="combinator">+</span>
-					<span class="letter">B</span>
-				</div>
-				<div class="widgetgroup">
-					<span class="label">Language</span>
-					<span class="value">Hindi, Marathi</span>
-				</div>
-				<div class="widgetgroup">
-					<span class="label">Usercase:</span>
-					<span class="value">TheWord</span>
-				</div>
-			</div>
-		</li>								
 	</ul>
 </div>
